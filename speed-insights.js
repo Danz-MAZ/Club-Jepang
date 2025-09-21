4c83faa30709ecead61b7ee7df67f67a0052b3e2
Danz-MAZ <sipzone4@gmail.com>
2025-09-21 15:56:16 +0700

    update build script and other changes

---
+++ speed-insights.js
@@ -0,0 +1,5 @@
+const { SpeedInsights } = require("@vercel/speed-insights/next");
+
+// Use the SpeedInsights class to interact with the package
+const speedInsights = new SpeedInsights();
+// Add your usage code here
